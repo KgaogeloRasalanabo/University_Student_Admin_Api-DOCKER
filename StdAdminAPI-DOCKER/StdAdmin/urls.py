f48a59f801d8f64a8ed236ac0e9c93627bf500be
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-03-14 12:18:19 +0200

    updated urls

--- StdAdminAPI-DOCKER/StdAdmin/urls.py
+++ StdAdminAPI-DOCKER/StdAdmin/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('SdtAdminApp.urls')),
+    path('<int:pk>/',include('SdtAdminApp.urls')),
 ]
